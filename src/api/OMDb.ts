b25435e3481314fe457b1a0cf9d53f730bbaa70a
Armianto Sumitro <sumitro@ualberta.ca>
2021-01-11 11:57:54 -0700

    Process omdb results to custom type

---
+++ src/api/OMDb.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import Movie from '../data/Movie'
+
+const apiKey = 'e287055f'
+const endpoint = `http://www.omdbapi.com`
+
+export async function searchMovie(title: string): Promise<Movie[]> {
+    const res = await axios.get(endpoint, {
+        params: {
+            apiKey,
+            type: 'movie',
+            s: title,
+        },
+    })
+
+    return res.data.Search.map((movie: any) => ({
+        title: movie.Title,
+        year: movie.Year,
+    }))
+}
