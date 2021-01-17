19927c7ae2376b56418a00a28d827da89fc8b485
Armianto Sumitro <sumitro@ualberta.ca>
2021-01-17 04:12:30 -0700

    Integrate with redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './redux/reducer';
 import './index.css';
 
-const store = createStore(reducer);
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = createStore(persistedReducer as any);
+const persistor = persistStore(store);
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
